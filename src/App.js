ba247a6227f87a158d10f9cb014113fb97f7fba8
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-04-29 02:28:04 +0600

    Routes are created for different components and 404 not found page created

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import { Button } from "react-bootstrap";
 import Navigation from "./components/Navigation/Navigation";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
+import Inventory from "./components/Inventory/Inventory";
+import About from "./components/About/About";
+import Blogs from "./components/Blogs/Blogs";
+import Register from "./components/Register/Register";
+import Login from "./components/Login/Login";
+import NotFound from "./components/NotFound/NotFound";
 
 function App() {
   return (
@@ -12,6 +18,12 @@ function App() {
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/inventory" element={<Inventory></Inventory>}></Route>
+        <Route path="/About" element={<About></About>}></Route>
+        <Route path="/blogs" element={<Blogs></Blogs>}></Route>
+        <Route path="/register" element={<Register></Register>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
     </div>
   );
