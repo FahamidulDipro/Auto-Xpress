f7d43e3167c636d4bd438b083b1e4540437bd835
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-04-29 02:17:59 +0600

    Active link added

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 import './Navigation.css';
 const Navigation = () => {
     return (
-        <Navbar bg="light" expand="lg">
+        <Navbar bg="dark" variant='dark' expand="lg">
   <Container>
-    <Navbar.Brand href="#home" className='fw-bold'> <span className='text-danger'>Auto</span> Xpress</Navbar.Brand>
+    <Navbar.Brand to="#home" className='fw-bold'> <span className='text-danger'>Auto</span> Xpress</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="me-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#link">Link</Nav.Link>
+        <NavLink to="/" className={({isActive})=>isActive?'active-link':'link'}>Home</NavLink>
+        <NavLink to="/inventory"  className={({isActive})=>isActive?'active-link':'link'}>Inventory</NavLink>
+        <NavLink to="/blogs"  className={({isActive})=>isActive?'active-link':'link'}>Blogs</NavLink>
+        <NavLink to="/about"  className={({isActive})=>isActive?'active-link':'link'}>About</NavLink>
         
       </Nav>
+      <Nav>
+      <NavLink to="/login"  className={({isActive})=>isActive?'active-link':'link'}>Login</NavLink>
+      <NavLink   to="/register"  className={({isActive})=>isActive?'active-link':'link'}>
+        Register
+      </NavLink>
+    </Nav>
     </Navbar.Collapse>
   </Container>
 </Navbar>
