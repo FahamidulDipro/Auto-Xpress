2f40357aaf7002d0085af10ba5e16ce22782b025
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-04-29 12:48:12 +0600

    Delivered button and Add stock input form added

--- src/components/InventoryDetail/InventoryDetail.js
+++ src/components/InventoryDetail/InventoryDetail.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Card } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import useInventory from "../../hooks/useInventory";
+import { useForm } from "react-hook-form";
 
 const InventoryDetail = () => {
   const inventories = useInventory();
@@ -9,18 +10,52 @@ const InventoryDetail = () => {
   const selectedInventory = inventories.find(
     (inventory) => inventory._id === inventoryId
   );
+  const [carQuantity, setCarQuantity] = useState(0);
+  const quantityDecreaser = () => {
+    if (carQuantity > 0) {
+      setCarQuantity(carQuantity - 1);
+    }
+  };
+  //Form submit functionalities
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => {
+    setCarQuantity(data.quantity);
+    console.log(data.quantity);
+  };
   return (
-    <div className="d-flex justify-content-between container border mt-5 w-75 p-3 text-start">
+    <div className="d-flex justify-content-between container border mt-5 w-75 p-3 text-start flex-lg-row flex-md-row flex-sm-column flex-column">
       <section className="">
         <img
           src={selectedInventory?.img}
           alt="inventoryImg"
-          className="img-fluid"
+          className="img-fluid mb-5"
         />
       </section>
       <section className="ms-5">
         <h2>{selectedInventory?.name}</h2>
         <p> {selectedInventory?.shortDescription}</p>
+        <p>
+          <b>Price: </b>${selectedInventory?.price}
+        </p>
+        <p>
+          <b>Quantity:</b> {carQuantity}
+        </p>
+        <div className="d-flex flex-lg-row flex-md-row flex-sm-column flex-column">
+          <div
+            className="me-3"
+            style={{ marginTop: "36px", marginBottom: "30px" }}
+          >
+            <Button variant="success" onClick={quantityDecreaser}>
+              Delivered
+            </Button>
+          </div>
+
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <h3>Restock items</h3>
+            <input type="number" {...register("quantity")} className="w-75" />
+            <input type="submit" value="+" className="ms-2" />
+          </form>
+        </div>
       </section>
     </div>
   );
