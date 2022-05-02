d7174e99737e9a0b51128f8f0a5a4effb75e727b
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-05-02 21:22:11 +0600

    Canceling any item from My Item page

--- src/components/MyItems/MyItems.js
+++ src/components/MyItems/MyItems.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Button, Col, Figure, Row } from "react-bootstrap";
 import { useAuthState } from "react-firebase-hooks/auth";
 import auth from "../../firebase.init";
 
@@ -10,22 +11,74 @@ const MyItems = () => {
       .then((res) => res.json())
       .then((data) => setSelectedItems(data));
   }, []);
+  //Canceling items
+  const cancelHandler = (id) => {
+    const confirmDelete = window.confirm("Are you sure you want to remove?");
+
+    if (confirmDelete) {
+      fetch(`http://localhost:5000/deleteItem/${id}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((result) => {
+          const remainingItems = selectedItems.filter(
+            (item) => item._id !== id
+          );
+          setSelectedItems(remainingItems);
+          console.log(result);
+        });
+    }
+  };
+
   return (
     <div style={{ marginTop: "100px" }}>
-      {selectedItems.map((selectedItem) => {
-        if (selectedItem.email === user?.email) {
-          return (
-            <div key={selectedItem._id}>
-              <h2>{selectedItem.name}</h2>
+      <Row>
+        {selectedItems.map((selectedItem) => {
+          if (selectedItem.email === user?.email) {
+            return (
+              <Col
+                lg={6}
+                key={selectedItem._id}
+                className="d-flex justify-content-center"
+              >
+                <Figure className="d-flex justify-content-between shadow-lg p-3 w-75 position-relative">
+                  <div className="d-flex  ">
+                    <Figure.Image
+                      width={300}
+                      height={280}
+                      alt="171x180"
+                      src={selectedItem.img}
+                    />
 
-              <p>
-                <b>Quantity: </b>
-                {selectedItem.quantity}
-              </p>
-            </div>
-          );
-        }
-      })}
+                    <Figure.Caption className="ms-3 text-start">
+                      <h2>{selectedItem.name}</h2>
+                      <p>
+                        <b>Short Description: </b>
+                        {selectedItem.shortDescription}
+                      </p>
+                      <p>
+                        <b>Price: </b>${selectedItem.price}
+                      </p>
+                      <p>
+                        <b>Quantity: </b>
+                        {selectedItem.quantity}
+                      </p>
+                    </Figure.Caption>
+                  </div>
+                  <div>
+                    <Button
+                      className="float-end position-absolute bottom-0 end-0 m-3 bg-secondary"
+                      onClick={() => cancelHandler(selectedItem._id)}
+                    >
+                      Cancel
+                    </Button>
+                  </div>
+                </Figure>
+              </Col>
+            );
+          }
+        })}
+      </Row>
     </div>
   );
 };
